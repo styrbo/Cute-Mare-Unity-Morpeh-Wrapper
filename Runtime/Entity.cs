ae7c7b1342c246d8c128dd5b2ba5c1f71a2d95eb
styrbo <styrboCanel@gmail.com>
2023-04-12 10:30:03 +0300

    u

--- Runtime/Entity.cs
+++ Runtime/Entity.cs
@@ -10,18 +10,9 @@ namespace CuteMareMorpeh
     [RequireComponent(typeof(EntityProvider))]
     public class Entity : EntityProvider
     {
-        private FastList<Collider> _onTriggerEnterColliders;
-
-        private void OnValidate()
-        {
-            if (GetComponent<EntityProvider>() == null)
-                gameObject.AddComponent<EntityProvider>();
-        }
 
         protected override void Initialize()
         {
-            _onTriggerEnterColliders = new FastList<Collider>(2);
-
             base.Initialize();
 
             Entity.GetComponent<GameObjectReferencesComponent>(out var exist);
@@ -30,40 +21,6 @@ namespace CuteMareMorpeh
                 return;
 
             Entity.AddComponent<GameObjectReferencesComponent>();
-
-            var rb = CreateRbComponent();
-
-            var stash = World.Default.GetStash<GameObjectReferencesComponent>();
-
-            stash.Set(Entity, new GameObjectReferencesComponent()
-            {
-                GameObject = gameObject,
-                Rigidbody = rb,
-            });
-        }
-
-        [CanBeNull]
-        private Rigidbody CreateRbComponent()
-        {
-            var rb = GetComponent<Rigidbody>();
-
-            if (rb == null)
-                return null;
-
-            var stash = World.Default.GetStash<PhysicBody>();
-
-            stash.Set(Entity, new PhysicBody()
-            {
-                Position = rb.position,
-                Rotation = rb.rotation,
-            });
-
-            return rb;
-        }
-
-        private void OnDestroy()
-        {
-            World.Default.RemoveEntity(Entity);
         }
     }
 }
